270e2b7f210c9358e90c38602075bc19f4955c44
Piyush Jain 8 <piyush.jain@publicissapient.com>
2019-10-15 15:55:56 +0200

    Added recursive to try multiple times. Sorted the result string.

--- SmallestString/Program.cs
+++ SmallestString/Program.cs
@@ -1,21 +1,38 @@
 ﻿using System;
+using System.Linq;
 
 namespace SmallestString
 {
     class Program
     {
-        static string result = string.Empty;
+        static string results = string.Empty;
         static void Main(string[] args)
+        {            
+            Run();            
+            Console.Read();
+        }
+
+        private static void Run()
         {
+            ulong weight;
+            string result = string.Empty;
             ulong[] sum = GetSumOfCharacters();
             char[] characters = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
             Console.WriteLine("Enter the weight?");
-            ulong weight =  ulong.Parse(Console.ReadLine());
-            string result = CalculateWeight(weight, sum, characters);
-
-            Console.WriteLine($"The result for weight {weight} is {result}");
-            Console.Read();
-                         
+            weight = ulong.Parse(Console.ReadLine());
+            result = CalculateWeight(weight, sum, characters);
+            Console.WriteLine($"The result for weight {weight} is {String.Concat(result.OrderBy(c => c))}");
+            Console.WriteLine("Do you want to try again. Press the character 'Y' to run again!");
+            var input = Console.ReadLine();
+            if (input.Equals('Y'.ToString()) || input.Equals('y'.ToString()))
+            {
+                results = string.Empty;
+                Run();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
 
         private static string CalculateWeight(ulong weight, ulong[] sum, char[] characters)
@@ -32,9 +49,9 @@ namespace SmallestString
                 }
             }
             ulong difference = weight - lastValue;
-            result += characters[counter - 2].ToString();
+            results += characters[counter - 2].ToString();
             if (difference != 0) CalculateWeight(difference,sum,characters);
-            return result;
+            return results;
         }       
         
         
